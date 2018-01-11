69ef62b3a2e675867056f7723c475bb1b9495c8f
haksaya <harunaksaya@marmara.edu.tr>
2018-01-11 14:44:38 +0300

    son hali

---
+++ xml_olusturma/WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace xml_olusturma
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string yol;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            link.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (XmlWriter xWr = XmlWriter.Create(Server.MapPath("library.xml")))
+            {
+                xWr.WriteStartDocument();
+
+                xWr.WriteStartElement("Library");
+
+
+                SqlConnection con = new SqlConnection("Server=den1.mssql5.gear.host;Database=kutuphane;User Id=kutuphane;Password = Wm8Na!iLw5_Q;");
+                con.Open();
+                SqlCommand komut = new SqlCommand();
+                komut.Connection = con;
+                komut.CommandText = "select * from books";
+                SqlDataReader dr = komut.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    xWr.WriteStartElement("List");
+
+                    // ADD FEW ELEMENTS.
+                    xWr.WriteElementString("BookName", dr["book_name"].ToString());
+                    xWr.WriteElementString("Category", dr["category"].ToString());
+                    xWr.WriteElementString("Price", dr["price"].ToString() + " TL");
+                    xWr.WriteEndElement();          // CLOSE LIST.
+                }
+
+                xWr.WriteEndElement();          // CLOSE LIBRARY.
+                xWr.WriteEndDocument();         // END DOCUMENT.
+
+                // FLUSH AND CLOSE.
+                xWr.Flush();
+                xWr.Close();
+            }
+            link.Visible = true;
+            yol = "http://xml.gearhostpreview.com/library.xml";
+        }
+    }
+}
---
+++ xml_olusturma/obj/Release/Package/PackageTmp/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="xml_olusturma.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Aktarım Merkezi</h2>
+            <p>Veri tabanı kayıtlarını XML'e aktar</p>
+            <asp:Button ID="Button1" runat="server" Text="XML'i Hazırla" CssClass="btn btn-success" OnClick="Button1_Click" />
+        </div>
+       <br>
+        <div class="container" id="link" runat="server">
+             <a href="<% Response.Write(yol); %>" class="btn btn-warning" role="link">İndir</a>
+        </div>
+    </form>
+</body>
+</html>
